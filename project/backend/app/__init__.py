a5ffe73a3d526ff27dc9cc9fd05699e9bf378ace
Meedloch <olivier.chabaut@gmail.com>
2024-11-24 16:54:08 +0100

    [FIX] Fixed popup bug

--- project/backend/app/__init__.py
+++ project/backend/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)  # Activer CORS pour toutes les routes
 
-    # Configuration MySQL
+    # Configuration de la base de données
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@db/reservations'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
