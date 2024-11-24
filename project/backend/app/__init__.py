b4b6189aa0e23e243c5dedc05dccd4b3a6f679c1
Meedloch <olivier.chabaut@gmail.com>
2024-11-24 20:24:22 +0100

    [FIX] Configured flask cors

--- project/backend/app/__init__.py
+++ project/backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +13,9 @@ def create_app():
     # Configuration de la base de données
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@db/reservations'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Activer CORS
+    CORS(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
