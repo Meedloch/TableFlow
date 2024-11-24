cb18402a088219ff6619b23bedd66f01561dadb7
Meedloch <olivier.chabaut@gmail.com>
2024-11-24 14:32:55 +0100

    [ADD] Flask migrate

--- project/backend/app/__init__.py
+++ project/backend/app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
-
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
-    # Configuration de la base de données avec MySQL Connector
+    # Configuration de la base de données
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@db/reservations'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)  # Initialiser Flask-Migrate avec l'application et la base de données
 
     # Importer les routes
     from app.routes import bp
